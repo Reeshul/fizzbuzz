0b569649b93af666ad4d4bf69486dd14be5bf627
hacaravan <hacavanagh@gmail.com>
2020-12-17 11:02:49 +0000

    Altered multiple of 3 test and drew up skeleton for other tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,6 +1,26 @@
 require 'fizzbuzz'
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
+  it 'returns "fizz" when passed a multiple of 3 that isn\'t a multple of 5' do
+    # Generate a random multiple of 3
+    test_input = (rand(6) + 1) * 3
+
+    # For this multiple of 3, run the test
+    while true do
+      break if test_input % 15 != 0
+      test_input = (rand(6) + 1) * 3
+    end
+    expect(fizzbuzz(test_input)).to eq 'fizz'
   end
+
+  # it 'returns "fizzbuzz" when a multiple of 15 is passed' do
+  #   # generate multiple of 15
+  #   # check this is .to eq 15
+  # end
+  #
+  # it 'returns "buzz" when a multiple of 5 that isn\'t a multiple of 3 is passed' do
+  #
+  # end
+  #
+  # it 'returns the number when passed a number that isn\'t a multiple of 3 or 5' do
+# end
 end
